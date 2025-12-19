b936d87bb2f14e80480f5360af14f46dd6223d8d
Drakthor475 <canterodiego17@gmail.com>
2025-12-19 17:51:39 -0600

    cambios header

---
+++ app/Componentes/body.tsx
@@ -0,0 +1,13 @@
+export default function BodyPage(){
+    interface gamecard{
+    
+    }
+    
+    return(
+        <section className="Games">
+            <h3 className="title" >Favorite Games</h3>
+            <hr/>
+        </section>
+    );
+
+}
--- app/View/page.tsx
+++ app/View/page.tsx
@@ -1,9 +1,11 @@
+import BodyPage from "../Componentes/body";
 import Header from "../Componentes/header";
 
 export default function MyProfile(){
     return(
         <div className="page">
             <Header/>
+            <BodyPage/>
             
         </div>
     );
