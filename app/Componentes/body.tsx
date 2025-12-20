756b7a014984a314963105ba544a8c84b302d141
Drakthor475 <canterodiego17@gmail.com>
2025-12-19 22:03:27 -0600

    Cards games

--- app/Componentes/body.tsx
+++ app/Componentes/body.tsx
@@ -5,8 +5,36 @@ export default function BodyPage(){
     
     return(
         <section className="Games">
-            <h3 className="title" >Favorite Games</h3>
-            <hr/>
+            <h2 className="title" >Favorite Games</h2>
+            <hr className="separador"/>
+            <div id="card">
+                <div className="wrapper-card">
+                    <div className="box-area">
+                        <div className="box">
+                        <img src={"./csgo.jpg"} alt="csgo image"/>
+                        <div className="overlay">
+                            <h3 className="title-card">Counter Strike 2</h3>
+                            <p className="des-card">CSGO is one of my favorite games because it requires aiming skills, teamwork, and strategy, and I enjoy improving and winning matches.</p>
+                        </div>
+                        </div>
+                        <div className="box">
+                        <img src={"./border.jpg"} alt="borderlands2 image"/>
+                        <div className="overlay">
+                            <h3 className="title-card">Borderlands 2</h3>
+                            <p className="des-card">Borderlands 2 is one of my favorite games because it's my childhood game, this game means a lot to me, I love this game.</p>
+                        </div>
+                        </div>
+                        <div className="box">
+                        <img src={"./fornai.jpg"} alt="Fortnite image"/>
+                        <div className="overlay">
+                            <h3 className="title-card">Fortnite</h3>
+                            <p className="des-card">Fortnite is one of my favorite games because it's fun to play with my brothers, I enjoy the gameplay and collecting skins and emotes.</p>
+                        </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </section>
     );
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import MyProfile from "./View/page";
 
 
 export default function Home() {
-  return (
-        <MyProfile/>
-      
+  return (    
+    <MyProfile/>
+
   );
 }
